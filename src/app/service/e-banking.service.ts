fa69fee198a7836d2e1fba8a055583ebf726cae1
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-11-12 12:38:45 -0600

    getAccountDetails

--- src/app/service/e-banking.service.ts
+++ src/app/service/e-banking.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Customer} from '../home/home.component';
 import {Observable} from 'rxjs';
+import {AccountDetails} from '../account/account.component';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,9 @@ export class EBankingService {
 
   loginUser(customer: Customer): Observable<Customer>{
     return this.http.post<Customer>(this.url + 'login', customer);
+  }
+
+  getAccountDetails(userName: string): Observable<AccountDetails> {
+    return this.http.post<AccountDetails>(this.url + 'getAccountDetails', userName);
   }
 }
