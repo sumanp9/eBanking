7b50b7f61b24817d5d19160493faa42c28775fc6
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-11-19 13:34:22 -0600

    Added path to savings account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AccountComponent} from './account/account.component';
+import {SavingsAccountComponent} from './savings-account/savings-account.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'account', component: AccountComponent}
+  {path: 'account', component: AccountComponent},
+  {path: 'savings', component: SavingsAccountComponent}
   ];
 
 @NgModule({
