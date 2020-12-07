63f9c2aede1a666ba5127a8059713f37b18b069d
Suman Pradhab <11.sumanpradhan@gmail.com>
2020-12-07 13:12:44 -0600

    Added route to CheckingAccount

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AccountComponent} from './account/account.component';
 import {SavingsAccountComponent} from './savings-account/savings-account.component';
+import {CheckingAccountComponent} from './checking-account/checking-account.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'account', component: AccountComponent},
-  {path: 'savings', component: SavingsAccountComponent}
+  {path: 'savings', component: SavingsAccountComponent},
+  {path: 'checking', component: CheckingAccountComponent}
+  //{path: '404', component: NotFoundComponent} : Future
   ];
 
 @NgModule({
