8143b9e063a9e757550ece5f2c30119137a64c3c
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-01-08 11:09:37 -0600

    used pipe functionality to convert amount to currency type.

--- src/app/transaction-history/transaction-history.component.ts
+++ src/app/transaction-history/transaction-history.component.ts
@@ -1,7 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {EBankingService, TransactionHistory} from '../service/e-banking.service';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {newArray} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-transaction-history',
