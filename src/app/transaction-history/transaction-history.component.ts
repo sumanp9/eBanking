a23c388b60c192d7937834ef2571133041736164
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-01-08 10:28:04 -0600

    Created Transaction History component

---
+++ src/app/transaction-history/transaction-history.component.ts
@@ -0,0 +1,32 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {EBankingService, TransactionHistory} from '../service/e-banking.service';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {newArray} from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-transaction-history',
+  templateUrl: './transaction-history.component.html',
+  styleUrls: ['./transaction-history.component.scss']
+})
+export class TransactionHistoryComponent implements OnInit {
+
+  accountTypeId: number;
+  transactionsHistory: Array<TransactionHistory>;
+  displayedColumns: string[];
+
+  constructor(@Inject(MAT_DIALOG_DATA) accountTypeId: number,
+              private service: EBankingService) {
+    this.accountTypeId = accountTypeId;
+    this.displayedColumns = ['Date', 'ToAccount', 'FromAccount', 'Amount'];
+  }
+
+  ngOnInit(): void {
+    this.refreshPage();
+  }
+
+  private refreshPage(): void {
+    this.service.getTransactionHistory('SAVINGS', this.accountTypeId).subscribe((transactions: TransactionHistory[]) => {
+      this.transactionsHistory = transactions;
+    });
+  }
+}
